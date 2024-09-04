80d3930f5e5ac08cd2112b142bb37774908f4409
Subodh20482 <149533665+Subodh20482@users.noreply.github.com>
2024-09-04 21:23:22 +0530

    create library

--- vitebasic/src/main.jsx
+++ vitebasic/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
+}
 
+const ReactElement ={
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        traget: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>visit google</a>
+)
 createRoot(document.getElementById('root')).render(
  
-    <App />
+    anotherElement
   
 )
+ 
