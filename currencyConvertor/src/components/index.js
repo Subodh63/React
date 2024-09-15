009b0b2ded366047b35b9c0aa62b2c6e371bd636
Subodh20482 <149533665+Subodh20482@users.noreply.github.com>
2024-09-15 23:05:00 +0530

    currency app

---
+++ currencyConvertor/src/components/index.js
@@ -0,0 +1,5 @@
+import InputBox from "./InputBox";
+
+export {
+    InputBox
+}
---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import {
+    useEffect,
+    useState
+} from "react"
+
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
