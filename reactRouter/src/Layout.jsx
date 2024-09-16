07fea81ae80d681836f018565b96c4d6815c521d
Subodh20482 <149533665+Subodh20482@users.noreply.github.com>
2024-09-16 23:01:57 +0530

    rectRouter

---
+++ reactRouter/src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+   
+    <Outlet />
+    
+    </>
+  )
+}
+
+export default Layout
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './assets/components/Home/Home.jsx'
+import About from './assets/components/About/About.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "About",
+        element: <About/>
+      },
+      
+     ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
